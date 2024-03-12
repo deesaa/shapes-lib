c1f32472b1a346d2346c3daf85a8d9299e8c8003
deesaa <deemsaer@gmail.com>
2024-03-12 17:05:29 +0400

    upd: sql task

---
+++ ProductsBase/SQL/fill_data.sql
@@ -0,0 +1,64 @@
+﻿GO
+DROP TABLE IF EXISTS dbo.ItemToCategory
+DROP TABLE IF EXISTS dbo.Categories
+DROP TABLE IF EXISTS dbo.Items
+DROP PROCEDURE IF EXISTS dbo.SetCategoryToItem;
+
+
+CREATE TABLE dbo.Items (
+    item_id INT NOT NULL IDENTITY (1, 1),
+    item_name VARCHAR (255) NOT NULL,
+    PRIMARY KEY (item_id)
+)
+
+CREATE TABLE dbo.Categories (
+    category_id INT NOT NULL IDENTITY (1, 1),
+    category_name VARCHAR (255) NOT NULL,
+    PRIMARY KEY (category_id)
+)
+
+CREATE TABLE dbo.ItemToCategory (
+    item_id INT,
+    category_id INT,
+    FOREIGN KEY (item_id) REFERENCES dbo.Items(item_id),
+    FOREIGN KEY (category_id) REFERENCES dbo.Categories(category_id),
+)
+
+INSERT INTO dbo.Items(item_name) VALUES
+    ('Apple'), ('Orange'), ('Cucumber'),
+    ('PlayStation'), ('Laptop'), ('Xiaomiphone'),
+    ('Glass'), ('Wood'), ('Diamond'),
+    ('Rock'), ('Paper'), ('Scissors');
+
+INSERT INTO dbo.Categories(category_name) VALUES
+    ('Cheap'), ('Gaming'), ('MustHave');
+
+GO;
+CREATE PROCEDURE dbo.SetCategoryToItem
+    @item_name VARCHAR (255), @category_name VARCHAR (255)
+AS
+INSERT INTO dbo.ItemToCategory(item_id, category_id)
+SELECT item_id, category_id FROM dbo.Items, dbo.Categories
+WHERE item_name = @item_name AND category_name = @category_name;
+
+GO;
+EXEC SetCategoryToItem @item_name = 'Apple', @category_name = 'Cheap';
+EXEC SetCategoryToItem @item_name = 'Orange', @category_name = 'Cheap';
+EXEC SetCategoryToItem @item_name = 'Cucumber', @category_name = 'Cheap';
+EXEC SetCategoryToItem @item_name = 'Apple', @category_name = 'MustHave';
+EXEC SetCategoryToItem @item_name = 'Orange', @category_name = 'MustHave';
+EXEC SetCategoryToItem @item_name = 'Cucumber', @category_name = 'MustHave';
+
+EXEC SetCategoryToItem @item_name = 'PlayStation', @category_name = 'Gaming';
+EXEC SetCategoryToItem @item_name = 'Laptop', @category_name = 'Gaming';
+EXEC SetCategoryToItem @item_name = 'Xiaomiphone', @category_name = 'Gaming';
+EXEC SetCategoryToItem @item_name = 'PlayStation', @category_name = 'MustHave';
+EXEC SetCategoryToItem @item_name = 'Laptop', @category_name = 'MustHave';
+EXEC SetCategoryToItem @item_name = 'Xiaomiphone', @category_name = 'MustHave';
+
+EXEC SetCategoryToItem @item_name = 'Rock', @category_name = 'Gaming';
+EXEC SetCategoryToItem @item_name = 'Paper', @category_name = 'Gaming';
+EXEC SetCategoryToItem @item_name = 'Scissors', @category_name = 'Gaming';
+EXEC SetCategoryToItem @item_name = 'Rock', @category_name = 'Cheap';
+EXEC SetCategoryToItem @item_name = 'Paper', @category_name = 'Cheap';
+EXEC SetCategoryToItem @item_name = 'Scissors', @category_name = 'Cheap';
---
+++ ProductsBase/SQL/query.sql
@@ -0,0 +1,7 @@
+﻿SELECT item_name, category_name
+FROM dbo.Items
+LEFT JOIN 
+    (SELECT item_name inner_item_name, category_name
+     FROM dbo.Items i, dbo.Categories c, dbo.ItemToCategory itc
+     WHERE i.item_id = itc.item_id AND c.category_id = itc.category_id) b 
+        ON b.inner_item_name = item_name
